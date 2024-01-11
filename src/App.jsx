5b369be1511b345378385d9a176714dea77a6084
rezis-work <139157419+rezis-work@users.noreply.github.com>
2024-01-11 14:33:43 +0400

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,63 @@
+import { useState } from "react";
+
+const messages = [
+  "Learn React ⚛️",
+  "Apply for jobs 💼",
+  "Invest your new income 🤑",
+];
+
+function App() {
+  const [step, setStep] = useState(1);
+  const [isOpen, setIsOpen] = useState(true);
+
+  function handlePrevious() {
+    if (step > 1) setStep((s) => s - 1);
+  }
+
+  function handleNext() {
+    if (step < 3) setStep((s) => s + 1);
+  }
+
+  function handleClose() {
+    setIsOpen((is) => !is);
+  }
+  return (
+    <>
+      <button className="close" onClick={handleClose}>
+        &times;
+      </button>
+      {isOpen && (
+        <div className="steps">
+          <div className="numbers">
+            <div className={`${step >= 1 ? "active" : ""}`}>1</div>
+            <div className={`${step >= 2 ? "active" : ""}`}>2</div>
+            <div className={`${step >= 3 ? "active" : ""}`}>3</div>
+          </div>
+
+          <p className="message">
+            Step {step}: {messages[step - 1]}
+          </p>
+
+          <div className="buttons">
+            <button
+              onClick={handlePrevious}
+              style={{ backgroundColor: "#7950f2", color: "#ffffff" }}
+              // disabled={step === 1}
+            >
+              Previous
+            </button>
+            <button
+              onClick={handleNext}
+              style={{ backgroundColor: "#7950f2", color: "#ffffff" }}
+              // disabled={step === 3}
+            >
+              Next
+            </button>
+          </div>
+        </div>
+      )}
+    </>
+  );
+}
+
+export default App;
